a328b58efbdf350fcd2f96654d34e8c281377471
Chloester2021 <nicolerobin1p@yahoo.com>
2021-10-09 16:32:14 -0700

    Add procfile and gitignore files

--- app.js
+++ app.js
@@ -125,6 +125,13 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function () {
   console.log("At your service 😊");
 });
